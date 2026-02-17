86a58317a27873f31ef52291e8087c8cf3d9fc43
Chelsea Martin <chemar3412@students.ecpi.edu>
2026-02-16 22:30:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ window.onload = function () {
 
     case false:
       uniDiv.style.display = "none";
-      personalDiv.style.display = "block"
+      personalDiv.style.display = "block";
       break;
   }
-};
+
+const skills = [ "HTML", "JavaScript", "DevC++", "C#", "Python", "SQL"];
+  const skillsList = document.getElementById("skillsList");
+
+  for ( let i = 0; i < skills.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = skills[i];
+    skillsList.appendChild(li);
+  });
